7773a9ea0d15e66646da3bbff585cebde13ecb95
Esteban <estebandpisani@gmail.com>
2022-06-03 17:07:51 -0300

    Asignación de props a Boton (destructuring) y eventos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import Boton from './components/Boton';
 
 function App() {
+
+  const manejarClick = () =>{
+    console.log("Click");
+  }
+
+  const reiniciarContador = () =>{
+
+  }
+
+
   return (
     <div className="App">
       <div className='contenedor-principal'>
-        
+        <Boton texto='Click' esBotonDeClick={true} manejarClick={manejarClick} />
+        <Boton texto='Reiniciar' esBotonDeClick={false} manejarClick={reiniciarContador}/>
+
 
       </div>
     </div>
--- src/components/Boton.jsx
+++ src/components/Boton.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
-//En vez de recibir el objeto 'props' como parámetro, directamente indicamos qué propiedades de ese props {} queremos recibir. Esta sintaxis se llama Destructuring. 
-function Boton({texto, esBotonDeClick}){
+//En vez de recibir el objeto 'props' como parámetro, directamente indicamos qué propiedades de ese props {} queremos recibir. Esta sintaxis se llama Destructuring.
+//Recibimos un   string,    boolean,   función.
+function Boton({texto, esBotonDeClick, manejarClick}){
     return (
 
         //Con un operador ternario evalúo el booleano "esBotonDeClick". Según su valor (true o false) le asigno una clase u otra.
-        <button className={esBotonDeClick ? "boton-click" : "boton-reiniciar"}>
+        <button className={esBotonDeClick ? "boton-click" : "boton-reiniciar"}
+        onClick={manejarClick}>
             {texto}
         </button>
     )
 }
+
+export default Boton;
