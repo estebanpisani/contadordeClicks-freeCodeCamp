e9e153609cdcf5d1aaf9004f63dddbcb280515c9
Esteban <estebandpisani@gmail.com>
2022-06-03 17:33:49 -0300

    Se añadió hook useState para manejar evento de clicks (sumar clicks y reiniciar). Fin del proyecto.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import './App.css';
 import Boton from './components/Boton';
 import Contador from './components/Contador';
+import { useState } from 'react';
 
 function App() {
 
-  const manejarClick = () =>{
-    console.log("Click");
+  const [numClicks, setNumClicks] = useState(0);
+
+  const sumarClick = () =>{
+    setNumClicks(numClicks + 1);
   }
 
   const reiniciarContador = () =>{
-  console.log("Reiniciar");
+    setNumClicks(0);
   }
 
 
   return (
     <div className="App">
       <div className='contenedor-principal'>
-        <Contador numClicks='5' />
-        <Boton texto='Click' esBotonDeClick={true} manejarClick={manejarClick} />
+        <Contador numClicks={numClicks} />
+        <Boton texto='Click' esBotonDeClick={true} manejarClick={sumarClick} />
         <Boton texto='Reiniciar' esBotonDeClick={false} manejarClick={reiniciarContador}/>
 
 
